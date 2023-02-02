ca4e045e1f6416c2c480aee94e3aed69430a3115
Brett Wolmarans <itsbrett@gmail.com>
2023-02-01 17:32:32 -0800

    good

--- frontend/widgets-app-frontend.py
+++ frontend/widgets-app-frontend.py
@@ -2,16 +2,16 @@ import requests
 from random import randint
 
 i = randint(1, 5)
-r =requests.get('http://127.0.0.1:3000/quantity/' + str(i))
+r =requests.get('http://widgets-widget:3000/widget/' + str(i))
 t = r.text
 print(t)
 
 i = randint(1, 5)
-r =requests.get('http://127.0.0.1:3001/widget/' + str(i))
+r =requests.get('http://widgets-quantity:3001/quantity/' + str(i))
 t = r.text
 print(t)
 
 i = randint(1, 5)
-r =requests.get('http://127.0.0.1:3002/warehouse/' + str(i))
+r =requests.get('http://widgets-warehouse:3002/warehouse/' + str(i))
 t = r.text
 print(t)
